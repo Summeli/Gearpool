a60aa412b827543fe69b028456e0dc5c3d6d45e9
Antti Pohjola <summeli@summeli.com>
2021-10-22 12:29:57 +0300

    yarn upgrade

--- reservation-app/src/components/footer.tsx
+++ reservation-app/src/components/footer.tsx
@@ -6,6 +6,7 @@ const Footer: React.FunctionComponent = () => {
     return (
         <div className="footer">
             <GitHubButton href="https://github.com/summeli" data-size="large" data-show-count="true" aria-label="Follow @summeli on GitHub">Follow @summeli</GitHubButton>
+            <GitHubButton href="https://github.com/summeli/trad-reservation" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star summeli/trad-reservation on GitHub">Star</GitHubButton>
             <GitHubButton href="https://github.com/summeli/trad-reservation/fork" data-icon="octicon-repo-forked" data-size="large" data-show-count="true" aria-label="Fork summeli/trad-reservation on GitHub">Fork</GitHubButton>
         </div>);
   };
