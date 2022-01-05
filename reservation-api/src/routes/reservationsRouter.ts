c5000baa495bd621110e93e3c4eb22f72941e1f8
Antti Pohjola <summeli@summeli.com>
2022-01-05 21:36:33 +0200

    Update reservationsRouter.ts

--- reservation-api/src/routes/reservationsRouter.ts
+++ reservation-api/src/routes/reservationsRouter.ts
@@ -1,14 +1,12 @@
 import {Router, Request, Response} from "express";
-import { verifyAdmin,verifyLogin } from "../middleware/loginmiddleware";
+import { verifyLogin } from "../middleware/loginmiddleware";
 
 const reservationsRouter = Router();
-
-
-const inventoryName: string = "trad"
 
 reservationsRouter.get("/",verifyLogin, async (_req: Request, res: Response) => {
 
     return res.status(200).send();
 });
+
 
 export default reservationsRouter;
