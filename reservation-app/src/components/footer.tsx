15d6b1c5d1239a25866beeb8abf45ed2099752a5
Antti Pohjola <summeli@summeli.com>
2022-05-06 13:37:24 +0300

    updated copyright

--- reservation-app/src/components/footer.tsx
+++ reservation-app/src/components/footer.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
+import GitHubButton from 'react-github-btn';
 
 const Footer: React.FunctionComponent = () => {
 
-    return ( <div className="footer"></div>
-
-    );
-    /*
     return (
         <div className="footer">
-            <GitHubButton href="https://github.com/summeli" data-size="large" data-show-count="true" aria-label="Follow @summeli on GitHub">Follow @summeli</GitHubButton>
-            <GitHubButton href="https://github.com/summeli/trad-reservation" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star summeli/trad-reservation on GitHub">Star</GitHubButton>
-            <GitHubButton href="https://github.com/summeli/trad-reservation/fork" data-icon="octicon-repo-forked" data-size="large" data-show-count="true" aria-label="Fork summeli/trad-reservation on GitHub">Fork</GitHubButton>
+            <p>Copyright 2020 Antti Pohjola.</p>
         </div>);
-        */
   };
   
   export default Footer;
